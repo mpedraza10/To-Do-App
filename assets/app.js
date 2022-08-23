f32efe5b832a77328e0069f0aea2a5d5a9d936df
Miguel Pedraza <mikeped469@gmail.com>
2022-08-23 13:32:39 -0500

    Improved list style with a radio button

--- assets/app.js
+++ assets/app.js
@@ -4,10 +4,23 @@ function addTask() {
     // Save the input task element in a variable
     const newTaskInputElement = document.getElementById('new-task-input');
     
-    // Create a new li and make its innerText to the value that is in the input
-    const newTaskListItem = document.createElement('li');    
-    newTaskListItem.innerText = newTaskInputElement.value;
-    
+    // Create a new li 
+    const newTaskListItem = document.createElement('li');
+
+    // Create a new label element for the task and change its innerText to the value that is in the input
+    const newTaskListLabel = document.createElement('label');
+    newTaskListLabel.setAttribute('for', 'task');
+    newTaskListLabel.innerText = newTaskInputElement.value;
+
+    // Create an input element with the type radio 
+    const newTaskRadioListInput = document.createElement('input');
+    newTaskRadioListInput.setAttribute('type', 'radio');
+    newTaskRadioListInput.setAttribute('id', 'task');
+
+    // Append tu the ul the input and label to the li created
+    newTaskListItem.appendChild(newTaskRadioListInput);
+    newTaskListItem.appendChild(newTaskListLabel);
+
     // Save the ul in a variable and append a new child which is the new list item created
     const taskListElement = document.getElementById('tasks-list');
     taskListElement.appendChild(newTaskListItem);
