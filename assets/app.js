fa6e5aae2ffd978e008c59abf1527b4252a6ae05
Miguel Pedraza <mikeped469@gmail.com>
2022-08-26 17:08:49 -0500

    Fixed: adding empty task now fixed

--- assets/app.js
+++ assets/app.js
@@ -19,37 +19,40 @@ function deleteTask(taskToDelete) {
 
 // Function to add and create a new list item, the task, and put it in the ul
 function addTask() {
-    // Create a new li 
-    const newTaskListItem = document.createElement('li');
+    // Check if the input is empty or not
+    if (newTaskInputElement.value != "") {
+        // Create a new li 
+        const newTaskListItem = document.createElement('li');
 
-    // Create a new label element for the task and change its innerText to the value that is in the input
-    const newTaskListLabel = document.createElement('label');
-    newTaskListLabel.setAttribute('for', 'task');
-    newTaskListLabel.innerText = newTaskInputElement.value;
+        // Create a new label element for the task and change its innerText to the value that is in the input
+        const newTaskListLabel = document.createElement('label');
+        newTaskListLabel.setAttribute('for', 'task');
+        newTaskListLabel.innerText = newTaskInputElement.value;
 
-    // Create an input element with the type radio 
-    const newTaskRadioListInput = document.createElement('input');
-    newTaskRadioListInput.setAttribute('type', 'checkbox');
-    newTaskRadioListInput.setAttribute('onClick', 'updateStatus(this)');
-    newTaskRadioListInput.setAttribute('class', 'task');
+        // Create an input element with the type radio 
+        const newTaskRadioListInput = document.createElement('input');
+        newTaskRadioListInput.setAttribute('type', 'checkbox');
+        newTaskRadioListInput.setAttribute('onClick', 'updateStatus(this)');
+        newTaskRadioListInput.setAttribute('class', 'task');
 
-    // Create a button to delete the task when completed
-    const deleteTaskBtn = document.createElement('button');
-    deleteTaskBtn.setAttribute('id', 'delete-task-btn');
-    deleteTaskBtn.setAttribute('onClick', 'deleteTask(this)');
-    deleteTaskBtn.innerText = "X"
+        // Create a button to delete the task when completed
+        const deleteTaskBtn = document.createElement('button');
+        deleteTaskBtn.setAttribute('id', 'delete-task-btn');
+        deleteTaskBtn.setAttribute('onClick', 'deleteTask(this)');
+        deleteTaskBtn.innerText = "X"
 
-    // Append tu the ul the input and label to the li created
-    newTaskListItem.appendChild(newTaskRadioListInput);
-    newTaskListItem.appendChild(newTaskListLabel);
-    newTaskListItem.appendChild(deleteTaskBtn);
+        // Append tu the ul the input and label to the li created
+        newTaskListItem.appendChild(newTaskRadioListInput);
+        newTaskListItem.appendChild(newTaskListLabel);
+        newTaskListItem.appendChild(deleteTaskBtn);
 
-    // Save the ul in a variable and append a new child which is the new list item created
-    const taskListElement = document.getElementById('tasks-list');
-    taskListElement.appendChild(newTaskListItem);
+        // Save the ul in a variable and append a new child which is the new list item created
+        const taskListElement = document.getElementById('tasks-list');
+        taskListElement.appendChild(newTaskListItem);
 
-    // Delete everything from the input
-    newTaskInputElement.value = "";
+        // Delete everything from the input
+        newTaskInputElement.value = "";
+    }
 }
 
 // Call event listener on the button if it is clicked we add new task using the function addTask
@@ -57,7 +60,9 @@ addTaskBtn.addEventListener('click', addTask);
 
 // Added event lister if the user hits enter instead of using the button
 newTaskInputElement.addEventListener('keyup', addEnterKey => { 
-    if(addEnterKey.key == "Enter") {
-        addTask();
+    if (newTaskInputElement.value != "") {
+        if(addEnterKey.key == "Enter") {
+            addTask();
+        }
     }
 });
